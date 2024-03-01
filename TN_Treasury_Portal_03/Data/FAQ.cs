34f6edf5889b95f8c913c20ea45187fdbe7e770e
jonas <jonas@C4LC1F3R>
2024-02-29 21:48:17 -0500

    Add project files.

---
+++ TN_Treasury_Portal_03/Data/FAQ.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TN_Treasury_Portal_03.Data
+{
+    public class FAQ
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Link { get; set; } = "";
+
+    }
+}
---
+++ TN_Treasury_Portal_03/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace TN_Treasury_Portal_03.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
