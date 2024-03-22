d5a94cc4d09ac7567b72873d0a17d3311f1f1ab0
Jonas Kotarski <jonaskotarski@gmail.com>
2024-03-22 01:38:29 -0400

    Added Employee page

---
+++ TN_Treasury_Portal_03/Data/TrainingItem.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TN_Treasury_Portal_03.Data
+{
+    public class TrainingItem
+    {
+        //Components user can add to FAQ object
+        [Required]
+        public static int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = "";
+        [Required]
+        public string Description { get; set; } = "";
+        public string Link { get; set; } = "";
+                   
+
+        //Putting all HTML components together to create the TrainingItem element
+        public override string ToString()
+        {
+            Id += 1;
+            //Formatting components to be added onto the page
+            string TitleHTML = "<h3>" + Title + "</h3><br>";
+            string DescriptionHTML = "<p>" + Description + "</p><br>";
+            string LinkHTML = "<a href= " + Link + ">View Link Here</a>";
+            
+            return "<div class=TrainingItem id=TrainingItem" + Id + ">" + TitleHTML + DescriptionHTML + LinkHTML;
+        }
+    }
+}
