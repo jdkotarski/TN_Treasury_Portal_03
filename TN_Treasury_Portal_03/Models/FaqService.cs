0476c75ea49c64110572bf5cf040ae33499dba29
Jonas Kotarski <jonaskotarski@gmail.com>
2024-04-21 23:05:01 -0400

    Added Create and Delete functionality to FAQs and Employee Training Items.

--- TN_Treasury_Portal_03/Models/FaqService.cs
+++ TN_Treasury_Portal_03/Models/FaqService.cs
@@ -11,15 +11,21 @@ namespace TN_Treasury_Portal_03.Models
             _context = context;
         }
         // Creates FAQ object in database with given user input
-        public async void CreateFaq(string NewTitle, string NewDescription, string NewLink)
+        public async Task<Faq[]> CreateFaq(string NewTitle, string NewDescription, string NewLink)
         {
+            Faq[] faqObjects;
             SqlParameter paramTitle = new SqlParameter("@NewTitle", NewTitle);
-            
             SqlParameter paramDescription = new SqlParameter("@NewDescription", NewDescription);
-
             SqlParameter paramLink = new SqlParameter("@NewLink", NewLink);
 
-            _context.Faqs.FromSqlRaw("Execute dbo.CreateFaq @NewTitle,@NewDescription,@NewLink", paramTitle, paramDescription, paramLink);
+            // Put in this useless try/catch to avoid "unhandled exception" popup on browser
+            try
+            {
+                faqObjects = _context.Faqs.FromSqlRaw("Execute dbo.CreateFaq @NewTitle,@NewDescription,@NewLink", paramTitle, paramDescription, paramLink).ToArray();
+            }
+            catch (Exception ex) { faqObjects = null; }
+            
+            return faqObjects;
         }
         // Retrieves all FAQ object titles, descriptions, and links from database
         public async Task<Faq[]> ReadFaq()
@@ -29,7 +35,7 @@ namespace TN_Treasury_Portal_03.Models
             return faqObjects;
         }
         // Updates FAQ object in database with given user input
-        public async void UpdateFaq(int TargetId, string NewTitle, string NewDescription, string NewLink)
+        public async Task UpdateFaq(int TargetId, string NewTitle, string NewDescription, string NewLink)
         {
             SqlParameter paramId = new SqlParameter("@TargetId", TargetId);
 
@@ -42,11 +48,19 @@ namespace TN_Treasury_Portal_03.Models
             _context.Faqs.FromSqlRaw("Execute dbo.EditFaq @TargetId,@NewTitle,@NewDescription,@NewLink", paramId, paramTitle, paramDescription, paramLink);
         }
         // Deletes specified FAQ object from database
-        public async void DeleteFaq(int TargetId)
+        public async Task<Faq[]> DeleteFaq(int? TargetId)
         {
-            SqlParameter paramId = new SqlParameter("@TargetId", TargetId);
+            Faq[] faqObjects;
+            SqlParameter paramTargetId = new SqlParameter("@TargetId", TargetId);
+            
+            // Put in this useless try/catch to avoid "unhandled exception" popup on browser
+            try
+            {
+                faqObjects = _context.Faqs.FromSqlRaw("Execute dbo.DeleteFaq @TargetId", paramTargetId).ToArray();
+            }
+            catch (Exception ex) { faqObjects = null; }
 
-            _context.Faqs.FromSqlRaw("Execute dbo.DeleteFaq @TargetId", paramId);
+            return faqObjects;
         }
     }
 }
--- TN_Treasury_Portal_03/Models/TrainingItemService.cs
+++ TN_Treasury_Portal_03/Models/TrainingItemService.cs
@@ -11,15 +11,21 @@ namespace TN_Treasury_Portal_03.Models
             _context = context;
         }
         // Creates TrainingItem object in database with given user input
-        public async void CreateTrainingItem(string NewTitle, string NewDescription, string NewLink)
+        public async Task<TrainingItem[]> CreateTrainingItem(string NewTitle, string NewDescription, string NewLink)
         {
+            TrainingItem[] trainingItemObjects;
             SqlParameter paramTitle = new SqlParameter("@NewTitle", NewTitle);
-
             SqlParameter paramDescription = new SqlParameter("@NewDescription", NewDescription);
-
             SqlParameter paramLink = new SqlParameter("@NewLink", NewLink);
 
-            _context.TrainingItems.FromSqlRaw("Execute dbo.CreateTrainingItem @NewTitle,@NewDescription,@NewLink", paramTitle, paramDescription, paramLink);
+            // Put in this useless try/catch to avoid "unhandled exception" popup on browser
+            try
+            {
+                trainingItemObjects = _context.TrainingItems.FromSqlRaw("Execute dbo.CreateTrainingItem @NewTitle,@NewDescription,@NewLink", paramTitle, paramDescription, paramLink).ToArray();
+            }
+            catch (Exception ex) { trainingItemObjects = null; }
+
+            return trainingItemObjects;
         }
         // Retrieves all TrainingItem object titles, descriptions, and links from database
         public async Task<TrainingItem[]> ReadTrainingItem()
@@ -42,11 +48,19 @@ namespace TN_Treasury_Portal_03.Models
             _context.TrainingItems.FromSqlRaw("Execute dbo.EditTrainingItem @TargetId,@NewTitle,@NewDescription,@NewLink", paramId, paramTitle, paramDescription, paramLink);
         }
         // Deletes specified TrainingItem object from database
-        public async void DeleteTrainingItem(int TargetId)
+        public async Task<TrainingItem[]> DeleteTrainingItem(int? TargetId)
         {
-            SqlParameter paramId = new SqlParameter("@TargetId", TargetId);
+            TrainingItem[] trainingItemObjects;
+            SqlParameter paramTargetId = new SqlParameter("@TargetId", TargetId);
 
-            _context.TrainingItems.FromSqlRaw("Execute dbo.DeleteTrainingItem @TargetId", paramId);
+            // Put in this useless try/catch to avoid "unhandled exception" popup on browser
+            try
+            {
+                trainingItemObjects = _context.TrainingItems.FromSqlRaw("Execute dbo.DeleteTrainingItem @TargetId", paramTargetId).ToArray();
+            }
+            catch (Exception ex) { trainingItemObjects = null; }
+
+            return trainingItemObjects;
         }
     }
 }
